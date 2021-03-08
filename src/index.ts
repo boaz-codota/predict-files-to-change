74cb7da5237917cd5ad205a9955d10c1ce2ad8c1
Boaz Berman <boaz@codota.com>
2021-03-08 14:50:17 +0200

    fixes

--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,38 @@
 import * as core from "@actions/core";
-import { getIssueContent } from "./getIssueContent";
-import { checkKeywords } from "./checkKeywords";
-import { setIssueLabel } from "./setIssueLabel";
-import { setIssueAssignee } from "./setIssueAssignee";
+import { context, getOctokit } from "@actions/github";
 
 async function run() {
   try {
-    core.setOutput("labeled", false.toString());
-    core.setOutput("assigned", false.toString());
-    const titleOrBody: string = core.getInput("title-or-body");
-    const token = core.getInput("github-token");
-    const content = await getIssueContent(token, titleOrBody);
-    const parameters: { keywords: string[], labels: string[], assignees: string[] }[] = JSON.parse(
-      core.getInput("parameters", {required: true})
-    );
-    if (!parameters) {
-      core.setFailed(
-        `parameters input not found. Make sure your ".yml" file contains a "parameters" JSON array like this:
-        parameters: '[ {"keywords": ["bug", "error"], "labels": ["BUG"], "assignees": ["username"]}, {"keywords": ["help", "guidance"], "labels": ["help-wanted"], "assignees": ["username"]}]'`
-      );
-    }
+    const token = core.getInput('github-token', {required: true})
+    // FIXME: DELETE ME
+    core.info(`Received token with length: "${token.length}"`)
 
-    const matchingKeywords = checkKeywords(parameters, content);
+    const client = getOctokit(token)
 
-    if (matchingKeywords === null) {
-      console.log("Keywords not included in this issue");
-      return;
-    } else {
-      setIssueLabel(token, matchingKeywords);
-      core.setOutput("labeled", true.toString());
-  
-      setIssueAssignee(token, matchingKeywords);
-      core.setOutput("assigned", true.toString());
-    }
+    const issue = await getIssue(client)
+    core.info(`Making a prediction of file for issue titled: "${issue.title}"`)
+    core.info(`Issue content: \n---\n${issue.body}\n---\n`)
+    client.issues.createComment({
+      ...context.repo,
+      issue_number: issue.issue_number,
+      body: 'Kaki pipi `23`'
+    })
   } catch (error) {
-    core.setFailed(error.message);
+    core.setFailed(error.message)
   }
+}
+
+async function getIssue(
+  client: any
+): Promise<{title: string; body: string; issue_number: number}> {
+  const issue_number = context.payload.issue?.number
+
+  const {data} = await client.issues.get({
+    ...context.repo,
+    issue_number
+  })
+
+  return {issue_number, ...data}
 }
 
 run();
